8669dfe4776e1022557d3b9ba36aa1e902034e25
gominada <gominada@proton.me>
2024-10-29 20:59:52 +0100

    Initialize Express project: install dependencies, setup index.js and app.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('node:http')
+require('dotenv').config()
+const app = require('./src/app')
+
+const port = (process.argv[2] || process.env.PORT || 3000)
+
+const server = http.createServer(app)
+
+server.listen(port)
+
+server.on('listening', () => {
+    console.log(`Server listening on http://localhost${port}`)
+})
+
+server.on('error', err => {
+    console.log(err)
+})
