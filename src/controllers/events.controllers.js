7f81e2222878a95be4b2dee38c5bfd5ad4570ba7
gominada <gominada@proton.me>
2024-10-30 19:10:53 +0100

    Add events controllers

--- src/controllers/events.controllers.js
+++ src/controllers/events.controllers.js
@@ -1,6 +1,72 @@
 const { Event } = require("../models/events.models")
-const { User } = require("../models/users.models")
 
 exports.testing = (req, res, next) => {
     console.log(req.url, "😝")
+}
+
+exports.getAll = async (req, res, next) => {
+    try {
+        const events = await Event.find()
+        res.json(events)
+    } catch (err) {
+        next(err)
+    }
+}
+
+exports.getById = async (req, res, next) => {
+    const { eventId } = req.params
+    try {
+        const event = await Event.findById(eventId)
+        res.json(event)
+    } catch (err) {
+        next(err)
+    }
+}
+
+exports.createEvent = async (req, res, next) => {
+    try {
+        const event = await Event.create(req.body)
+        res.status(201).json(event)
+    } catch (err) {
+        next(err)
+    }
+}
+
+exports.updateEvent = async (req, res, next) => {
+    const { eventId } = req.params
+    try {
+        const event = await Event.findByIdAndUpdate(eventId, req.body, { new: true })
+        res.json(event)
+    } catch (err) {
+        next(err)
+    }
+}
+
+exports.deleteEvent = async (req, res, next) => {
+    const { eventId } = req.params
+    try {
+        const event = await Event.findByIdAndDelete(eventId)
+        res.json(event)
+    } catch (err) {
+        next(err)
+    }
+}
+
+exports.getAllSortedByDate = async (req, res, next) => {
+    try {
+        const events = await Event.find().sort({ date: 1 })
+        res.json(events)
+    } catch (err) {
+        next(err)
+    }
+}
+
+exports.getByDate = async (req, res, next) => {
+    const { from, to } = req.query
+    try {
+        const events = await Event.find({ date: { $gte: from, $lte: to } })
+        res.json(events)
+    } catch (err) {
+        next(err)
+    }
 }
--- src/routes/api/events.routes.js
+++ src/routes/api/events.routes.js
@@ -1,16 +1,15 @@
-const { testing } = require('../../controllers/events.controllers')
-
 const router = require('express').Router()
+const { getById, getAll, createEvent, getAllSortedByDate, getByDate, updateEvent, deleteEvent } = require('../../controllers/events.controllers')
 
-router.get('/', testing)
-router.get('/date', testing)
-router.get('/upcoming', testing)
-router.get('/:eventId', testing)
+router.get('', getAll)
+router.get('/date', getByDate)
+router.get('/upcoming', getAllSortedByDate)
+router.get('/:eventId', getById)
 
-router.post('/', testing)
+router.post('/', createEvent)
 
-router.put('/:eventId', testing)
+router.put('/:eventId', updateEvent)
 
-router.delete('/:eventId', testing)
+router.delete('/:eventId', deleteEvent)
 
 module.exports = router
